ea1fee6465dcc43c507b352d755a3575a4a9de16
a104360 <a104360@alunos.uminho.pt>
2024-09-23 14:50:56 +0100

    aula 2 de Ia com grafos

---
+++ ia/aula2/DraftFicha1/Graph.py
@@ -0,0 +1,228 @@
+import math
+from collections import deque
+
+import networkx as nx  # biblioteca de tratamento de grafos necessária para desnhar graficamente o grafo
+import matplotlib.pyplot as plt  # idem
+
+from Node import Node
+
+
+# Constructor
+# Number of edges
+# Adjacancy matrix, adjacency list, list of edges
+
+# Methods for adding edges
+
+# Methods for removing edges
+
+# Methods for searching a graph
+# BFS, DFS, 
+
+
+class Graph:
+    def __init__(self, directed=False):
+        self.m_nodes = []  
+        self.m_directed = directed
+        self.m_graph = {}  # dicionario para armazenar os nodos e arestas
+
+    #############
+    #    escrever o grafo como string
+    #############
+    def __str__(self):
+        out = ""
+        for key in self.m_graph.keys():
+            out = out + "node" + str(key) + ": " + str(self.m_graph[key]) + "\n"
+            return out
+
+    ################################
+    #   encontrar nodo pelo nome
+    ################################
+
+    def get_node_by_name(self, name):
+        search_node = Node(name)
+        for node in self.m_nodes:
+            if node == search_node:
+                return node
+            else:
+                return None
+
+    ##############################3
+    #   imprimir arestas
+    ############################333333
+
+    def imprime_aresta(self):
+        listaA = ""
+        lista = self.m_graph.keys()
+        for nodo in lista:
+            for (nodo2, custo) in self.m_graph[nodo]:
+                listaA = listaA + nodo + " ->" + nodo2 + " custo:" + str(custo) + "\n"
+        return listaA
+
+    ################
+    #   adicionar   aresta no grafo
+    ######################
+
+    def add_edge(self, node1, node2, weight):
+        n1 = Node(node1)
+        n2 = Node(node2)
+        if (n1 not in self.m_nodes):
+            n1_id = len(self.m_nodes)  # numeração sequencial
+            n1.setId(n1_id)
+            self.m_nodes.append(n1)
+            self.m_graph[node1] = []
+        
+
+        if (n2 not in self.m_nodes):
+            n2_id = len(self.m_nodes)  # numeração sequencial
+            n2.setId(n2_id)
+            self.m_nodes.append(n2)
+            self.m_graph[node2] = []
+        
+
+        self.m_graph[node1].append((node2, weight))  
+
+
+    #############################
+    # devolver nodos
+    ##########################
+
+    def getNodes(self):
+        return self.m_nodes
+
+    #######################
+    #    devolver o custo de uma aresta
+    ##############3
+
+    def get_arc_cost(self, node1, node2):
+        custoT = math.inf
+        a = self.m_graph[node1]  # lista de arestas para aquele nodo
+        for (nodo, custo) in a:
+            if nodo == node2:
+                custoT = custo
+
+        return custoT
+
+    ##############################
+    #  dado um caminho calcula o seu custo
+    ###############################
+
+    def calcula_custo(self, caminho):
+        # caminho é uma lista de nodos
+        teste = caminho
+        custo = 0
+        i = 0
+        while i + 1 < len(teste):
+            custo = custo + self.get_arc_cost(teste[i], teste[i + 1])
+            #print(teste[i])
+            i = i + 1
+        return custo
+
+    ################################################################################
+    #     procura DFS -- TO DO
+    ####################################################################################
+
+    def procura_DFS(self, start,end,path = [],visited = set()):
+        path.append(start)
+        visited.add(start)
+
+        if start == end:
+            return path
+
+        for (node,cost) in self.m_graph[start]:
+            if node not in visited:
+                return  self.procura_DFS(node,end,path,visited)
+
+    #####################################################
+    # Procura BFS  -- TO DO
+    ######################################################
+
+    def procura_BFS(self,start,end):
+
+        # Verify if the start and then end nodes are the same
+        if start == end:
+            return [start]
+        
+        # Inicialize visited set and queue
+        visited = set([start])
+        q = deque([start])
+
+        # The shortest paths to each node are stored in a dictionary
+        path = {start:[start]}
+
+        # While every node was not visited
+        while q:
+            node = q.popleft() # Take next node to visit
+
+            # For every adjacent node 
+            for (adjacent,cost) in self.m_graph[node]: 
+                
+                # If it was not visited
+                if adjacent not in visited: 
+
+                    # Add it to the visited nodes
+                    visited.add(adjacent)
+
+                    # Add the adjacent node to the (to be visited) queue
+                    q.append(adjacent)
+
+                    # Add the path to the dictionary
+                    path[adjacent] = path[node] + [adjacent]
+
+
+                    # If the node is the end/goal
+                    if adjacent == end:
+                        #return the path for the node
+                        return path[adjacent]
+
+        """for value in self.m_graph:
+            print(value)
+            for node in value:
+                if node == end:
+                    return path
+                if node not in visited:
+                    visited.add(node)
+                    continue"""
+
+                    
+
+    ####################
+    # função  getneighbours, devolve vizinhos de um nó
+    ##############################
+
+    def getNeighbours(self, nodo):
+        lista = []
+        for (adjacente, peso) in self.m_graph[nodo]:
+            lista.append((adjacente, peso))
+        return lista
+
+    ###########################
+    # desenha grafo  modo grafico
+    #########################
+
+    def desenha(self):
+        ##criar lista de vertices
+        lista_v = self.m_nodes
+        lista_a = []
+        g = nx.Graph()
+        for nodo in lista_v:
+            n = nodo.getName()
+            g.add_node(n)
+            for (adjacente, peso) in self.m_graph[n]:
+                lista = (n, adjacente)
+                # lista_a.append(lista)
+                g.add_edge(n, adjacente, weight=peso)
+
+        pos = nx.spring_layout(g)
+        nx.draw_networkx(g, pos, with_labels=True, font_weight='bold')
+        labels = nx.get_edge_attributes(g, 'weight')
+        nx.draw_networkx_edge_labels(g, pos, edge_labels=labels)
+
+        plt.draw()
+        plt.show()
+
+   
+
+
+
+
+
